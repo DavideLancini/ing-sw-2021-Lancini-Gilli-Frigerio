4b60ac12e609977cb7d8e1afc902bf623bc204fd
andrea gilli <10607527@polimi.it>
2021-03-22 15:01:22 +0100

    marble,market and takereasorce metod

---
+++ src/main/java/it/polimi/ingsw/Marble.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw;
+
+public enum Marble {
+    WHITE, BLUE, GRAY, YELLOW, PURPLE, RED, EMPTY;
+}
---
+++ src/main/java/it/polimi/ingsw/Market.java
@@ -0,0 +1,50 @@
+package it.polimi.ingsw;
+
+public class Market {
+
+    private Marble sideMarble;
+    private Marble[][] marketBoard = new Marble[4][3] ;
+    /** Return marble out of the market board  */
+    public Marble getSideMarble() {
+        return sideMarble;
+    }
+    /** Set marble out of the market board*/
+    public void setSideMarble(Marble sideMarble) {
+        this.sideMarble = sideMarble;
+    }
+    /** Show the current market board */
+    public Marble[][] getMarketBoard() {
+        return marketBoard;
+    }
+    /** set the starting market, use only on game start */
+    public void setMarketBoard(Marble[][] marketBoard) {
+        this.marketBoard = marketBoard;
+    }
+    /** take 3 or 4 marbles from current market and insert sideMarble in the
+     * new market
+     */
+    public Marble[] takeResources(boolean isRow,int position){
+        Marble[] taken=new Marble[4];
+        Marble oldSideMarble=sideMarble;
+        if(isRow){
+            for (int i=0;i<4;i++){
+                taken[i] = marketBoard[position][i];
+                if(i==0)
+                    setSideMarble(marketBoard[position][i]); //the first position drops and goes in sideMarble
+                else
+                    marketBoard[position][i-1]=marketBoard[position][i]; //left scrolling all Marbles in the row
+            }
+            getMarketBoard()[position][3]=getSideMarble();// insert the old sideMarble in the market
+            return taken;
+        }
+        for (int i=0;i<3;i++){
+            taken[i] = marketBoard[i][position];
+            if(i==0)
+                setSideMarble(marketBoard[i][position]); //the first position drops and goes in sideMarble
+                else
+                    marketBoard[i-1][position]=marketBoard[i][position]; //up scrolling all Marbles in the column
+            }
+        getMarketBoard()[position][2]=getSideMarble();
+        return taken;
+    }
+}
