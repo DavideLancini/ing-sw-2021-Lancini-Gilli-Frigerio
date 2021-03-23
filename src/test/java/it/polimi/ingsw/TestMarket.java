62f658328202f7ad478c8421d4fdd519157438d2
andrea gilli <10607527@polimi.it>
2021-03-23 11:41:05 +0100

    test correction

--- src/test/java/it/polimi/ingsw/TestMarket.java
+++ src/test/java/it/polimi/ingsw/TestMarket.java
@@ -8,18 +8,20 @@ public class TestMarket
 {
     @Test
     public void trySetMarbleWithCorrectValue() {
+        Market market = new Market();
         Marble testSide = Marble.WHITE;
-        Market.setSideMarble(testSide);
-        Marble returnedSide = Market.getSideMarble();
+        market.setSideMarble(testSide);
+        Marble returnedSide = market.getSideMarble();
 
         assertSame(testSide, returnedSide);
     }
     @Test
     public void trySetMarbleWithWrongValue()
     {
+        Market market = new Market();
         Marble testSide = Marble.EMPTY;
         try {
-            Market.setSideMarble(testSide);
+            market.setSideMarble(testSide);
         }
         catch (Exception ignored){
         }
