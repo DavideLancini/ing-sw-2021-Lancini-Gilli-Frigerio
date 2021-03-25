4bb2f5e0bcc94dd74c86cc9f1ca8ac41c6663337
Davide Lancini <davide1.lancini@mail.polimi.it>
2021-03-25 11:55:36 +0100

    Added DevCardDeck

---
+++ src/main/java/it/polimi/ingsw/DevCardDeck.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw;
+
+public class DevCardDeck {
+    // private DevCard[][][] decks;
+    // draw (color: CardColor, level: Level): DevCard
+    // getTopCards(): DevCard[3][4]
+}
