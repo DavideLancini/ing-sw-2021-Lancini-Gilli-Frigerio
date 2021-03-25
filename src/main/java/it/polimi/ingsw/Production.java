443c01b59b8bd48b60e948abc523d4f72677bcbe
Davide Lancini <davide1.lancini@mail.polimi.it>
2021-03-25 11:53:14 +0100

    Added Production

---
+++ src/main/java/it/polimi/ingsw/Production.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw;
+
+public class Production {
+    private Resource[] input;
+    private Resource[] output;
+
+    public Resource[] getInputProduction(){
+        return this.input;
+    }
+
+    public Resource[] getOutputProduction(){
+        return this.output;
+    }
+
+    public Resource[] produce(Resource[] inResource){
+        //TODO entire produce method
+    }
+}
