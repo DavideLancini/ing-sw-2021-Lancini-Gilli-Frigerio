d9495fa9269e452e7a52f467b8af5506204a71e5
andrea gilli <10607527@polimi.it>
2021-05-06 15:41:54 +0200

    javaDoc

--- src/main/java/it/polimi/ingsw/model/singlePlayer/ActionPile.java
+++ src/main/java/it/polimi/ingsw/model/singlePlayer/ActionPile.java
@@ -3,9 +3,17 @@ package it.polimi.ingsw.model.singlePlayer;
 import java.util.Collections;
 import java.util.Stack;
 
+/**
+ * class ActionPile
+ * @author Gruppo 12
+ */
 public class ActionPile {
     private static Stack<ActionToken> pile;
 
+    /**
+     * ShufflePile
+     * shuffle pile of ActionToken
+     */
     public static void ShufflePile(){
         Collections.shuffle(pile);
     }
--- src/main/java/it/polimi/ingsw/model/singlePlayer/ActionToken.java
+++ src/main/java/it/polimi/ingsw/model/singlePlayer/ActionToken.java
@@ -2,27 +2,51 @@ package it.polimi.ingsw.model.singlePlayer;
 
 import it.polimi.ingsw.model.CardColor;
 
+/**
+ * class ActionToken
+ * @author Gruppo 12
+ */
 public abstract class ActionToken {
 }
-
+/**
+ * class ActionFaith extends ActionToken
+ * @author Gruppo 12
+ */
 class ActionFaith extends ActionToken{
+    /**
+     * AddFaith
+     * add 2 to DarkFaith
+     */
      public void AddFaith(){
        int faith=DarkFaith.getDarkFaith();
        DarkFaith.setDarkFaith(faith+2);
     }
 }
-
+/**
+ * class ActionShuffle extends ActionToken
+ * @author Gruppo 12
+ */
 class ActionShuffle extends ActionToken{
+    /**
+     * AddFaith
+     * add 1 to DarkFaith and shuffle ActionPile
+     */
     public void AddFaith(){
         int faith=DarkFaith.getDarkFaith();
         DarkFaith.setDarkFaith(faith+1);
         ActionPile.ShufflePile();
     }
 }
-
+/**
+ * class ActionRemove extends ActionToken
+ * @author Gruppo 12
+ */
 class ActionRemove extends ActionToken{
     private CardColor cardColor;
 
+    /**
+     *
+     */
     public void RemoveDevCard(){
         //TODO: revome 2 devCard with same cardColor
     }
