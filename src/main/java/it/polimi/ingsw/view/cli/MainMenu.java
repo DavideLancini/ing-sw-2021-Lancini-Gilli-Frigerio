8f4c36bb8d37d765575cb8a293c20c65d777a96d
Davide Lancini <davide1.lancini@mail.polimi.it>
2021-05-11 20:38:58 +0200

    Syntax correction

--- src/main/java/it/polimi/ingsw/view/cli/MainMenu.java
+++ src/main/java/it/polimi/ingsw/view/cli/MainMenu.java
@@ -5,7 +5,7 @@ import it.polimi.ingsw.model.Market;
 public class MainMenu {
 
     public void showMenu(){
-        System.out.println( "1. Crate Match" );
+        System.out.println( "1. Create Match" );
         System.out.println( "2. Join Match" );
         System.out.println( "3. View Public Match" );
         System.out.println( "4. Create Custom Rule Set" );
