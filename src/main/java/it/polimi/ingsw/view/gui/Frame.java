782421b95d3f3e7aa7807277fb41780914bc9f09
andrea gilli <10607527@polimi.it>
2021-05-19 15:44:24 +0200

    gui setup

---
+++ src/main/java/it/polimi/ingsw/view/gui/Frame.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw.view.gui;
+
+
+
+import com.sun.glass.ui.Screen;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Frame extends JFrame {
+    ImageIcon image = new ImageIcon(
+            getClass().getResource("/Masters of Renaissance_PlayerBoard (11_2020)-1.png"));
+    Image scaledImg = image.getImage().getScaledInstance(500,320,Image.SCALE_REPLICATE);
+
+
+    public Frame(){
+        super("Masters Of Renaissance");
+        JLabel imageLabel = new JLabel( new ImageIcon(scaledImg));
+        imageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        imageLabel.setVerticalAlignment(SwingConstants.BOTTOM);
+        add(imageLabel);
+        setLayout(new GridLayout());
+        setSize(1920,1200);
+        setVisible(true);
+        setResizable(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+}
