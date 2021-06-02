da4eb0219233d0405c2fde90c2095f45d1d99af8
Davide Lancini <davide1.lancini@mail.polimi.it>
2021-06-02 12:16:51 +0200

    quickfix

---
+++ src/main/java/it/polimi/ingsw/network/messages/MessageLocalPort.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.network.messages;
+
+import it.polimi.ingsw.network.ClientMessage;
+
+public class MessageLocalPort extends ClientMessage {
+    private int localPort;
+
+    public MessageLocalPort(int localPort){
+        this.localPort=localPort;
+    }
+
+    public int getPort(){return localPort; }
+}
