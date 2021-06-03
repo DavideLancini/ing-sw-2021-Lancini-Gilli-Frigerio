a6ced0895226032032189c2084a4c3427bb699ff
Davide Lancini <davide1.lancini@mail.polimi.it>
2021-06-03 16:50:16 +0200

    javadoc and code cleanup

--- src/main/java/it/polimi/ingsw/network/components/Listener.java
+++ src/main/java/it/polimi/ingsw/network/components/Listener.java
@@ -7,57 +7,81 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Listener Component
+ * A Listener is a Socket that receive Strings from the Sender.
+ * It needs a fatherSocket (ServerSocket) to be initialized.
+ * All Listeners are opened on the same Port but create different InputStream
+ * All Listener shares the same logger (received from the initialization of the first Listener)
  *
  * @author Lancini Davide
  */
-public class Listener{
+public class Listener {
     private static Logger logger;
 
-    private Socket listenerSocket;
-    private DataInputStream inputStream;
-    private String listenerName;
+    private final Socket listenerSocket;
+    private final DataInputStream inputStream;
+    private final String listenerName;
 
-    public Listener(ServerSocket fatherSocket, Logger logger) throws DisconnectedException{
+    /**
+     * Constructor for the Listener the listener will be stuck on accept until a connection is made.
+     *
+     * @throws DisconnectedException when one of the function fail
+     * @author Lancini Davide
+     */
+    public Listener(ServerSocket fatherSocket, Logger logger) throws DisconnectedException {
         Listener.logger = logger;
-        logger.setLevel(Level.ALL);
         //Accept connection
         try {
             this.listenerSocket = fatherSocket.accept();
             this.listenerName = "Listener" + getTargetAddress();
-            logger.log(Level.INFO,this.listenerName+" has accepted a connection");
+            logger.info(this.listenerName + " has accepted a connection");
         } catch (IOException error) {
-            logger.log(Level.WARNING,"Cannot accept a connection");
+            logger.warning("Cannot accept a connection");
             throw new DisconnectedException("Listener cannot accept connection");
         }
         //Open input stream
         try {
             this.inputStream = new DataInputStream(new BufferedInputStream(this.listenerSocket.getInputStream()));
-            logger.log(Level.INFO,this.listenerName+"has created a stream ");
+            logger.info(this.listenerName + "has created a stream ");
         } catch (IOException error) {
-            logger.log(Level.WARNING,this.listenerName+"Listener failed to open a stream");
+            logger.warning(this.listenerName + "Listener failed to open a stream");
             throw new DisconnectedException("Failed to open a stream");
         }
     }
 
+    /**
+     * The Receive Method is the Main method used for the Listener.
+     * It tries to read one string from its inputStream.
+     *
+     * @return an empty string if there are no messages
+     * @throws DisconnectedException when the Stream is unavailable
+     * @author Lancini Davide
+     */
     public String receive() throws DisconnectedException {
         String rawMessage = "";
         try {
             rawMessage = this.inputStream.readUTF();
-            logger.log(Level.INFO, this.listenerName + "has received: "+rawMessage);
+            logger.info(this.listenerName + "has received: " + rawMessage);
             return rawMessage;
         } catch (IOException e) {
             throw new DisconnectedException("Failed to receive");
         }
     }
 
-    public String getTargetAddress(){
-        String temp = this.listenerSocket.getRemoteSocketAddress().toString();
-        temp = temp.substring(1,temp.indexOf(":"));
-        return temp;
+    /**
+     * getTarget Address collects the address from the socket (as an INetAddress), it casts it into a string and removes
+     * the "/" and the port number (after a ":")
+     * Ex.: /192.168.1.123:1001 -> 192.168.1.123
+     * It's only used by the Server Network to open a Sender to the right Client
+     *
+     * @author Lancini Davide
+     */
+    public String getTargetAddress() {
+        String address = this.listenerSocket.getRemoteSocketAddress().toString();
+        address = address.substring(1, address.indexOf(":"));
+        logger.info(this.listenerName + ": has been requested its target address:" + address);
+        return address;
     }
 }
