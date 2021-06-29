cf1e2cab84d3d86ec8e2ecad282f720641aa2663
Davide Lancini <davide1.lancini@mail.polimi.it>
2021-06-29 14:02:08 +0200

    Unified Logger

--- src/main/java/it/polimi/ingsw/view/Log.java
+++ src/main/java/it/polimi/ingsw/view/Log.java
@@ -1,5 +1,31 @@
 package it.polimi.ingsw.view;
 
-public class Log {
+import javax.lang.model.type.NullType;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+
+public class Log extends Thread{
     public static java.util.logging.Logger logger = java.util.logging.Logger.getLogger("MainLogger");
+
+    public static void saveLog(){
+        FileHandler file = null;
+        try {
+            file = new FileHandler("className.log");
+        } catch (IOException error) {
+            //Do Nothing
+        }
+        logger.addHandler(file);
+    }
+
+    public void run(){
+        while(true){
+            saveLog();
+            try {
+                sleep(2000);
+            } catch (InterruptedException error) {
+                saveLog();
+                break;
+            }
+        }
+    }
 }
